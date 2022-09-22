31d692e03957e0b6ed91e50a3bdb102959e70909
Simone Rubino <sir@takobi.online>
2022-09-22 18:16:10 +0200

    [IMP] website_repeat_sale: Simplify quantities management

--- website_repeat_sale/controllers/main.py
+++ website_repeat_sale/controllers/main.py
@@ -36,27 +36,18 @@ class WebSaleOrderRepeat(http.Controller):
         if sale_order.state != 'draft':
             request.session['sale_order_id'] = None
             sale_order = request.website.sale_get_order(force_create=True)
-        add_qty = 0
-        set_qty = 0
+
         for line1 in repeat_order_id.order_line:
             if line1.product_id:
-                if sale_order.order_line:
-                    for line2 in sale_order.order_line:
-                        if line2.product_id == line1.product_id:
-                            add_qty = line1.product_uom_qty + line2.product_uom_qty
-                            set_qty = add_qty
-                            break
-                        else:
-                            add_qty = line1.product_uom_qty
-                            set_qty = add_qty
+                for line2 in sale_order.order_line:
+                    if line2.product_id == line1.product_id:
+                        set_qty = line1.product_uom_qty + line2.product_uom_qty
+                        break
                 else:
-                    add_qty = line1.product_uom_qty
-                    set_qty = add_qty
+                    set_qty = line1.product_uom_qty
 
                 sale_order._cart_update(
                     product_id=int(line1.product_id.id),
-                    add_qty=add_qty,
                     set_qty=set_qty,
                 )
         return request.redirect("/shop/cart")
-
